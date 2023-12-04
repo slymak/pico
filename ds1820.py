857362fe3d590fd914959c66c25f72dd7743003f
slymak <slymak@gmail.com>
2023-12-04 18:05:22 +0000

    trying again

--- ds1820.py
+++ ds1820.py
@@ -6,14 +6,16 @@ led = machine.Pin(25, Pin.OUT)
 ds_sensor = ds18x20.DS18X20(onewire.OneWire(ds_pin))
 roms = ds_sensor.scan()
 
-while True:
-  ds_sensor.convert_temp()
-  time.sleep_ms(750)
- 
-  for rom in roms:
-    print('{0} : {1}'.format(binascii.hexlify(rom), ds_sensor.read_temp(rom)))
- 
-  time.sleep(1)
-  led.toggle()
-  time.sleep(4)
-  led.toggle()
+print('Found DS devices: ', roms)
+
+#while True:
+#ds_sensor.convert_temp()
+#time.sleep_ms(750)
+
+#for rom in roms:
+#  print('{0} : {1}'.format(binascii.hexlify(rom), ds_sensor.read_temp(rom)))
+
+#  time.sleep(1)
+#  led.toggle()
+#  time.sleep(4)
+#  led.toggle()
