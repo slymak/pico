78dc16de064cf32cf41e88eac9cb4a03cdb04601
slymak <slymak@gmail.com>
2023-12-07 14:59:37 +0000

    one line

--- ds1820.py
+++ ds1820.py
@@ -1,21 +1,18 @@
 import machine, onewire, ds18x20, time, binascii
 from machine import Pin
- 
-ds_pin = machine.Pin(17)
-led = machine.Pin(25, Pin.OUT)
+
+ds_pin = machine.Pin(16)
+#led = machine.Pin(25, Pin.OUT)
 ds_sensor = ds18x20.DS18X20(onewire.OneWire(ds_pin))
 roms = ds_sensor.scan()
 
-print('Found DS devices: ', roms)
+#print('Found DS devices: ', roms)
 
-#while True:
-#ds_sensor.convert_temp()
-#time.sleep_ms(750)
+ds_sensor.convert_temp()
+time.sleep_ms(750)
 
-#for rom in roms:
-#  print('{0} : {1}'.format(binascii.hexlify(rom), ds_sensor.read_temp(rom)))
-
-#  time.sleep(1)
-#  led.toggle()
-#  time.sleep(4)
-#  led.toggle()
+incol = []
+for rom in roms:
+##  print('{0} : {1}'.format(binascii.hexlify(rom), ds_sensor.read_temp(rom)))
+  incol.append('{0:.0f}'.format(ds_sensor.read_temp(rom)))
+print(*incol)
